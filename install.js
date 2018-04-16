ce10e3506c4b5e48a7100cfe92a6cc52996f74e7
Nate Sanders <nsanders@paypal.com>
2018-04-16 11:53:28 -0700

    Updating install.js

--- install.js
+++ install.js
@@ -1,52 +1,52 @@
-const exec = require('child-process-promise').exec;
+const exec = require('child-process-promise').exec
 
 const apps = [
-	{
-		name: 'pp-mp-order',
-		npm: true,
-		bower: true
-	}
-];
+			{
+				name: 'pp-mp-order',
+				npm: true,
+				bower: true
+			}
+		]
 
 function callback(err, result) {
 	if(err) {
-		console.log('-- AN ERROR OCCURRED --');
-		console.log(err);
+		console.log('-- AN ERROR OCCURRED --')
+		console.log(err)
 	} else {
 		if(result.npm === true) {
-			console.log(result.name + ' NPM packages installed successfully!');
+			console.log(result.name + ' NPM packages installed successfully!')
 		} else {
-			//no output
+			console.log('NO NPM packages to install!')
 		}
 		if(result.bower === true) {
-			console.log(result.name + ' BOWER packages installed successfully!');
+			console.log(result.name + ' BOWER packages installed successfully!')
 		} else {
-			//no output
+			console.log('NO BOWER packages to install!')
 		}
 	}
 }
 
-exec('npm install')
-	.then(function(result) {
-		console.log('pp-mp-examples NPM packages installed successfully');
-		apps.forEach(funcition (application) {
-			let command = 'cd ' + application.name + '; '
-			if(application.npm === true) {
+exec('npm install; npm install bower;')
+	.then((result) => {
+		console.log('pp-mp-examples NPM packages installed successfully')
+		apps.forEach((myApp) => {
+			let command  = 'cd ' + myApp.name + '; '
+			if(myApp.npm === true) {
 				command += 'npm install; '
 			}
-			if(application.bower === true) {
+			if(myApp.bower === true) {
 				command += 'bower install;'
 			}
 			exec(command)
-				.then(function (result) {
-					callback(null, application)
+				.then((result) => {
+					callback(null, myApp)
 				})
-				.catch(function (err) {
+				.catch((err) => {
 					callback(err)
-				})
-		})
+				});
+		});
 	})
-	.catch(function (err) {
+	.catch((err) => {
 		console.log('ERROR while installing packages for pp-mp-examples')
 		console.log(err)
-	});
+	})
